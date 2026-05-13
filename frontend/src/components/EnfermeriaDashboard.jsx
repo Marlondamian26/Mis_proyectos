e964d47cf1f09f9be10f877f888cc5856be44201
Marlondamian26 <monterreymorejonm@gmail.com>
2026-05-13 09:27:39 -0400

    Add useCallback import to EnfermeriaDashboard.jsx

--- frontend/src/components/EnfermeriaDashboard.jsx
+++ frontend/src/components/EnfermeriaDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import axiosInstance from '../services/auth'
 import { useNavigate } from 'react-router-dom'
 import { useLanguage } from '../context/LanguageContext'
